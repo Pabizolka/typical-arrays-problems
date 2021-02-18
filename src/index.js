6dc1290cedaa1d3c340ebabc91a6ab4a1bc3311a
Pabizolka <1232312009qq@gmail.com>
2021-02-18 17:30:07 +0300

    feat: comlete task (all tests passed)

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
+exports.min = function min(array) {
+    if (!array) return 0;
+    let res = array[0];
+    array.forEach((item) => {
+        res = item < res ? item : res;
+    });
+    return isNaN(res) ? 0 : res;
+};
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array) return 0;
+    let res = array[0];
+    array.forEach((item) => {
+        res = item > res ? item : res;
+    });
+    return isNaN(res) ? 0 : res;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (!array || array.length == 0) return 0;
+    let res = 0;
+    res = array.reduce((sum, current) => sum + current);
+    res = res / array.length;
+    return isNaN(res) ? 0 : res;
+};
